2b2c237617d9c8e5f0bd05af72ab46f50136c3b4
Mars <zm03322161@qq.com>
2023-02-22 00:00:35 +0800

    websocket

---
+++ app/src/main/java/com/zhang/autotouch/utils/StompUtils.java
@@ -0,0 +1,29 @@
+package com.zhang.autotouch.utils;
+
+import android.util.Log;
+
+
+import static com.zhang.autotouch.conf.Const.TAG;
+
+import ua.naiksoftware.stomp.StompClient;
+
+public class StompUtils {
+    @SuppressWarnings({"ResultOfMethodCallIgnored", "CheckResult"})
+    public static void lifecycle(StompClient stompClient) {
+        stompClient.lifecycle().subscribe(lifecycleEvent -> {
+            switch (lifecycleEvent.getType()) {
+                case OPENED:
+                    Log.d(TAG, "Stomp connection opened");
+                    break;
+
+                case ERROR:
+                    Log.e(TAG, "Error", lifecycleEvent.getException());
+                    break;
+
+                case CLOSED:
+                    Log.d(TAG, "Stomp connection closed");
+                    break;
+            }
+        });
+    }
+}
